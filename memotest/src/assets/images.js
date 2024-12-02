7eeff114d1c05b98167cd80008e5ac5550f6aa2e
Mauro Sandoval <mgsandoval2021@gmail.com>
2024-12-02 15:24:47 -0300

    agregadas card al tablero

---
+++ memotest/src/assets/images.js
@@ -0,0 +1,67 @@
+import bear from './bear.png';
+import beaver from './beaver.png';
+import bee from './bee.png';
+import boar from './boar.png';
+import butterfly from './butterfly.png';
+import cat from './cat.png';
+import chicken from './chicken.png';
+import cow from './cow.png';
+import crocodile from './crocodile.png';
+import dog from './dog.png';
+import elephant from './elephant.png';
+import fox from './fox.png';
+import frog from './frog.png';
+import giraffe from './giraffe.png';
+import hippopotamus from './hippopotamus.png';
+import koala from './koala.png';
+import lion from './lion.png';
+import monkey from './monkey.png';
+import mouse from './mouse.png';
+import octopus from './octopus.png';
+import owl from './owl.png';
+import penguin from './penguin.png';
+import pig from './pig.png';
+import rhinoceros from './rhinoceros.png';
+import rooster from './rooster.png';
+import seal from './seal.png';
+import shark from './shark.png';
+import sheep from './sheep.png';
+import squirrel from './squirrel.png';
+import tiger from './tiger.png';
+import turtles from './turtles.png';
+
+const images = [
+    { src: bear },
+    { src: beaver },
+    { src: bee },
+    { src: boar },
+    { src: butterfly },
+    { src: cat },
+    { src: chicken },
+    /*cow,
+    crocodile,
+    dog,
+    elephant,
+    fox,
+    frog,
+    giraffe,
+    hippopotamus,
+    koala,
+    lion,
+    monkey,
+    mouse,
+    octopus,
+    owl,
+    penguin,
+    pig,
+    rhinoceros,
+    rooster,
+    seal,
+    shark,
+    sheep,
+    squirrel,
+    tiger,
+    turtles*/
+]
+
+export default images;
--- memotest/src/components/Tablero/Tablero.jsx
+++ memotest/src/components/Tablero/Tablero.jsx
@@ -1,7 +1,29 @@
+import images from '../../assets/images.js';
+import './Tablero.css';
+import {useEffect, useState} from "react";
+
 const Tablero = () =>{
+    const [pairImages, setPairImages] = useState([]);
+
+    useEffect( () => {
+        setPairImages([...images, ...images]
+            .sort(() => Math.random() - 0.5)
+            .map((image, index) => ({...image, id: index, flipped: false})))
+    }, [])
+
+
     return(
-        <>Jugadores</>
+        <div className="container">
+            <h1>Memotest</h1>
+            <div className="tablero-container">
+                {pairImages.map((image) => (
+                    <div className="card-container">
+                        <img className="card-image" src={image.src} alt="imagen" key={image.id}/>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
-export default Tablero
+export default Tablero;
