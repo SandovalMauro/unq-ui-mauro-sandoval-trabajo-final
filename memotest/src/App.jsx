588d36fa31105221dd9dede50111e3fd4ef07388
Mauro Sandoval <mgsandoval2021@gmail.com>
2024-12-09 15:24:43 -0300

    eliminado import sin usar

--- memotest/src/App.jsx
+++ memotest/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {BrowserRouter as Router, useNavigate} from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
 import RouterApp from "./Router.jsx";
 
 
