81b0f9b538c7890754313d181c86bc669d440d26
Mauro Sandoval <mgsandoval2021@gmail.com>
2024-12-10 10:38:57 -0300

    seleccionar cantidades de cards

--- memotest/src/components/Menu/Menu.jsx
+++ memotest/src/components/Menu/Menu.jsx
@@ -1,8 +1,10 @@
 import {useNavigate} from "react-router-dom";
 import './Menu.css';
+import {useState} from "react";
 
 
 const Menu = () =>{
+    const [quantity, setQuantity] = useState(1);
     const navigate = useNavigate();
     const handlerRutaJ = () => {
         navigate("/jugadores");
@@ -13,9 +15,27 @@ const Menu = () =>{
     return(
         <div className="container">
             <p>Menu</p>
+            <div className="selection-player-container">
+                <p>Seleccione cantidad de jugadores</p>
+                <div className="buttons-player-container">
+                    <button className="button-player">1</button>
+                    <button className="button-player">2</button>
+                </div>
 
-            <p onClick={handlerRutaJ}>jugadores</p>
-            <p onClick={handlerRutaT}>tablero</p>
+            </div>
+            <div className="selection-cards-container">
+                <p>Seleccione la cantidad de pares</p>
+                <div>
+                    <select className="select-cant" value={quantity}
+                            onChange={(e) => setQuantity(Number(e.target.value))}>
+                        {[...Array(32).keys()].map(i => (
+                            <option key={i + 1}
+                                    value={i + 1}> {i + 1} {i + 1 > 1 ? " unidades" : " unidad"}</option>
+                        ))}
+                    </select>
+
+                </div>
+            </div>
 
             <button onClick={handlerRutaT}>Jugar</button>
         </div>
